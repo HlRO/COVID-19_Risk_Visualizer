e55e58f56624f3479488bba07a18057a9a297dd6
Hiro <ibot@jcom.home.ne.jp>
2020-04-08 07:17:45 +0000

    Update the download script

--- scripts/download_covid_stats.sh
+++ scripts/download_covid_stats.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 wget "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" -O $PROJECT_HOME"/data/usa/NYT_covid-19_data.csv"
 wget "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv" -O $PROJECT_HOME"/data/world/ECDC_covid-19_data.csv"
+cd $PROJECT_HOME/scripts
+./usa/preprocess_covid_data.py -i $PROJECT_HOME"/data/usa/NYT_covid-19_data.csv" -o $PROJECT_HOME"/data/usa/total_cases.csv"
+./world/preprocess_covid_data.py -i $PROJECT_HOME"/data/world/ECDC_covid-19_data.csv" -o $PROJECT_HOME"/data/world/total_cases.csv"
